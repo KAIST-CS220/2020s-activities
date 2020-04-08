b346db2f709b3e5ca0209b883be0e7f0e08b335c
Sang Kil Cha <sangkilc@kaist.ac.kr>
2020-04-08 10:22:41 +0900

    Update scores

--- 06/MyFunc.fs
+++ 06/MyFunc.fs
@@ -1,4 +1,9 @@
 module Activity06.MFIXME // FIXME = student id
 
+let rec computeLength lst =
+  match lst with
+  | Cons (_, tl) -> 1 + computeLength tl
+  | Nil -> 0
+
 let myfunc (lst: int MyList) = // DO NOT TOUCH THIS LINE
-  -1
+  computeLength lst
